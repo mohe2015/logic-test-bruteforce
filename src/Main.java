a735ddf9a9cfc3cc5173bbfa58cd29aadd28e1ab
mohe2015 <Moritz.Hedtke@t-online.de>
2021-01-03 21:47:42 +0100

    Maybe faster without exceptions

--- src/Main.java
+++ src/Main.java
@@ -42,112 +42,116 @@ public class Main {
 			System.out.println("--------------------------------");
 		}
 		
-		try {
-			field.reinitialize();
-			Codeline[] pcf = f0;
-			int pc = 0;
+		field.reinitialize();
+		Codeline[] pcf = f0;
+		int pc = 0;
 
-			for (int i = 0; i < 1000; i++) {
-				if (debug) {
-					System.out.println("f0: " + Arrays.toString(f0));
-					System.out.println("f1: " + Arrays.toString(f1));
-					System.out.println("pcf: " + Arrays.toString(pcf));
-					System.out.println("pc: " + pc + " pci: " + pcf[pc]);
-					System.out.println(field);
-					
-					String input = s.nextLine();
-					if (input.equals("s")) {
-						return;
-					}
-				}
+		for (int i = 0; i < 1000; i++) {
+			if (pc == pcf.length || field.posX == field.field.length || field.posY == field.field[0].length || field.posX < 0 || field.posY < 0) {
+				return;
+			}
+			
+			if (debug) {
+				System.out.println("f0: " + Arrays.toString(f0));
+				System.out.println("f1: " + Arrays.toString(f1));
+				System.out.println("pcf: " + Arrays.toString(pcf));
+				System.out.println("pc: " + pc + " pci: " + pcf[pc]);
+				System.out.println(field);
 				
-				switch (pcf[pc].condition) {
-					case UNCONDITIONAL: {
-						break;
-					}
-					case GREEN: {
-						if (field.field[field.posX][field.posY] != FieldType.GREEN && field.field[field.posX][field.posY] != FieldType.GREEN_WITH_X) {
-							pc++;
-							continue;
-						}
-						break;
-					}
-					case RED: {
-						if (field.field[field.posX][field.posY] != FieldType.RED) {
-							pc++;
-							continue;
-						}
-						break;
-					}
-				}
-				switch (pcf[pc].action) {
-					case CALL_F0: {
-						pcf = f0;
-						pc = 0;
-						break;
-					}
-					case CALL_F1: {
-						pcf = f1;
-						pc = 0;
-						break;
-					}
-					case FORWARD: {
-						field.posX += field.directionX;
-						field.posY += field.directionY;
-						
-						if (field.field[field.posX][field.posY] == FieldType.GREEN_WITH_X) {
-							field.field[field.posX][field.posY] = FieldType.GREEN;
-							field.foundCrosses++;
-							if (field.foundCrosses == 4) {
-								System.out.println("found solution "+Arrays.toString(f0)+" "+Arrays.toString(f1));
-								return;
-								//System.exit(0);
-							}
-						}
-						if (field.field[field.posX][field.posY] == FieldType.EMPTY) {
-							return;
-						}
-						pc++;
-						break;
-					}
-					case LEFT: {
-						// -1  0 ->  0  1
-						//  1  0 ->  0 -1
-						//  0 -1 -> -1  0
-						//  0  1 ->  1  0
-						
-						if (field.directionX == 0) {
-							field.directionX = field.directionY;
-							field.directionY = 0;
-						} else if (field.directionY == 0) {
-							field.directionY = -field.directionX;
-							field.directionX = 0;
-						}
-						
-						pc++;
-						break;
-					}
-					case RIGHT: {
-						// -1  0 ->  0 -1
-						//  1  0 ->  0  1
-						//  0 -1 ->  1  0
-						//  0  1 -> -1  0
-						
-						if (field.directionX == 0) {
-							field.directionX = -field.directionY;
-							field.directionY = 0;
-						} else if (field.directionY == 0) {
-							field.directionY = field.directionX;
-							field.directionX = 0;
-						}
-						
-						pc++;
-						break;
-					}
+				String input = s.nextLine();
+				if (input.equals("s")) {
+					return;
 				}
 			}
-		} catch (ArrayIndexOutOfBoundsException e) {
 			
+			switch (pcf[pc].condition) {
+				case UNCONDITIONAL: {
+					break;
+				}
+				case GREEN: {
+					if (field.field[field.posX][field.posY] != FieldType.GREEN && field.field[field.posX][field.posY] != FieldType.GREEN_WITH_X) {
+						pc++;
+						continue;
+					}
+					break;
+				}
+				case RED: {
+					if (field.field[field.posX][field.posY] != FieldType.RED) {
+						pc++;
+						continue;
+					}
+					break;
+				}
+			}
+			switch (pcf[pc].action) {
+				case CALL_F0: {
+					pcf = f0;
+					pc = 0;
+					break;
+				}
+				case CALL_F1: {
+					pcf = f1;
+					pc = 0;
+					break;
+				}
+				case FORWARD: {
+					field.posX += field.directionX;
+					field.posY += field.directionY;
+					
+					if (field.posX == field.field.length || field.posY == field.field[0].length || field.posX < 0 || field.posY < 0) {
+						return;
+					}
+					
+					if (field.field[field.posX][field.posY] == FieldType.GREEN_WITH_X) {
+						field.field[field.posX][field.posY] = FieldType.GREEN;
+						field.foundCrosses++;
+						if (field.foundCrosses == 4) {
+							System.out.println("found solution "+Arrays.toString(f0)+" "+Arrays.toString(f1));
+							return;
+							//System.exit(0);
+						}
+					}
+					if (field.field[field.posX][field.posY] == FieldType.EMPTY) {
+						return;
+					}
+					pc++;
+					break;
+				}
+				case LEFT: {
+					// -1  0 ->  0  1
+					//  1  0 ->  0 -1
+					//  0 -1 -> -1  0
+					//  0  1 ->  1  0
+					
+					if (field.directionX == 0) {
+						field.directionX = field.directionY;
+						field.directionY = 0;
+					} else if (field.directionY == 0) {
+						field.directionY = -field.directionX;
+						field.directionX = 0;
+					}
+					
+					pc++;
+					break;
+				}
+				case RIGHT: {
+					// -1  0 ->  0 -1
+					//  1  0 ->  0  1
+					//  0 -1 ->  1  0
+					//  0  1 -> -1  0
+					
+					if (field.directionX == 0) {
+						field.directionX = -field.directionY;
+						field.directionY = 0;
+					} else if (field.directionY == 0) {
+						field.directionY = field.directionX;
+						field.directionX = 0;
+					}
+					
+					pc++;
+					break;
+				}
+			}
 		}
 	}
 
